432f33a3603589d90f0b6150d19266b363953b58
badalpatel1274 <badalpatel31628@gmail.com>
2025-03-04 07:36:00 +0530

    logo addeddd.....

--- src/components/layout/UserNavbar.jsx
+++ src/components/layout/UserNavbar.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-// import {logo} from '../image/logo.png'
+import logo from '../image/logox.png'
 import '../css/userNavbar.css'
 const UserNavbar = () => {
     const [isOpen, setIsOpen] = useState(false);   
      return (
         <>
       <nav className="navbar">
-        <div className="navbar-logo">ResumeBuilder</div>
+        {/* <div className="navbar-logo">ResumeBuilder</div> */}
+        <img className='navbar-logo' src={logo} alt="" style={{width:"120px"}}/>
 
         {/* Hamburger Menu Icon */}
         <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
